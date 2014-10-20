65794a47146c4648280ae53acb2bbb77c5371b21
Surendran Mahendran <surenspost@gmail.com>
2014-10-20 00:27:43 -0400

    Send slack notification when order is completed

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -8,4 +8,19 @@ Spree::Order.class_eval do
       SlackNotificationWorker.perform_async(self.number)
     end
   end
+
+  def user_display_format
+    if name.present? && email.present?
+      return "#{name}(#{email})"
+    else
+      return "#{email}"
+    end
+  end
+
+  def slack_notification_message    
+    line_items_str = ""
+    self.line_items.each { |line_item| line_items_str += "\n#{line_item.variant.name}(#{line_item.quantity})" }
+    
+    "Order completed by #{user_display_format} for $#{self.total} with items #{line_items_str}"
+  end
 end
--- app/workers/slack_notification_worker.rb
+++ app/workers/slack_notification_worker.rb
@@ -1,8 +1,11 @@
+require 'slack-notifier'
+
 class SlackNotificationWorker
   include Sidekiq::Worker
 
   def perform(order_number)
-    puts "Notifying order state for #{order_number}"
     order = Spree::Order.find_by_number(order_number)
+    notifier = Slack::Notifier.new(ENV['SLACK_TEAM_NAME'], ENV['SLACK_CHANNEL_TOKEN'])
+    notifier.ping(order.slack_notification_message)
   end
 end
