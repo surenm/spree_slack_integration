e794f4297110c8dea45396740226f382b92f6739
Surendran Mahendran <surenspost@gmail.com>
2014-10-22 13:31:38 -0400

    Customizable channel for every post

--- app/workers/slack_notification_worker.rb
+++ app/workers/slack_notification_worker.rb
@@ -6,6 +6,6 @@ class SlackNotificationWorker
   def perform(order_number)
     order = Spree::Order.find_by_number(order_number)
     notifier = Slack::Notifier.new(ENV['SLACK_TEAM_NAME'], ENV['SLACK_CHANNEL_TOKEN'])
-    notifier.ping(order.slack_notification_message)
+    notifier.ping(order.slack_notification_message, channel: ENV['SLACK_CHANNEL_NAME'])
   end
 end
